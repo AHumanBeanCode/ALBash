34fde5fc6891f7eb9658e71a524b0c4ddbcfde18
AHumanBeanCode <124647544+AHumanBeanCode@users.noreply.github.com>
2025-04-20 16:15:15 +1200

    Update and rename VBox-Chrooted to VBox-Chrooted.sh

---
+++ VBox-Chrooted.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/bash
+
+echo "Generating Locales"
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+read -p "Enter Hostname: " HOSTNAME
+echo "$HOSTNAME" > /etc/hostname
+
+echo "Set root password"
+passwd
+
+echo "Installing Grub"
+pacman -Sy --noconfirm grub
+grub-install --target=i386-pc /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+
+#Finish
+echo "Please Reboot"
